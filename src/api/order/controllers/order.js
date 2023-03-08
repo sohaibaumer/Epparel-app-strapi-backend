47d5d6e52ae3056ffd02fdcac634a7e7e8a6926f
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-03-08 20:37:10 +0500

    fix item and order

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,13 +1,13 @@
-"use strict";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+'use strict';
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 /**
  * order controller
  */
 
-const { createCoreController } = require("@strapi/strapi").factories;
+const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
   async create(ctx) {
     const { products, userName, email } = ctx.request.body;
     try {
@@ -15,12 +15,12 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
       const lineItems = await Promise.all(
         products.map(async (product) => {
           const item = await strapi
-            .service("api::item.item")
+            .service('api::item.item')
             .findOne(product.id);
 
           return {
             price_data: {
-              currency: "usd",
+              currency: 'usd',
               product_data: {
                 name: item.name,
               },
@@ -33,24 +33,24 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
 
       // create a stripe session
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
+        payment_method_types: ['card'],
         customer_email: email,
-        mode: "payment",
-        success_url: "http://localhost:2001/checkout/success",
-        cancel_url: "http://localhost:2001",
+        mode: 'payment',
+        success_url: 'http://localhost:3000/checkout/success',
+        cancel_url: 'http://localhost:3000',
         line_items: lineItems,
       });
 
       // create the item
       await strapi
-        .service("api::order.order")
+        .service('api::order.order')
         .create({ data: { userName, products, stripeSessionId: session.id } });
 
       // return the session id
       return { id: session.id };
     } catch (error) {
       ctx.response.status = 500;
-      return { error: { message: "There was a problem creating the charge" } };
+      return { error: { message: 'There was a problem creating the charge' } };
     }
   },
 }));
